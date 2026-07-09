4f9120ecbf7dd7d1ebb2b3a74e6d0c96dc6dd99f
sinan-prvt <mohamedsinan9400@gmail.com>
2026-07-09 17:25:32 +0530

    Cleaned up temporary database migration files

--- backend/import_postgres.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo "Migrating new Postgres database..."
-python manage.py migrate
-
-echo "Loading data from db_backup.json into Postgres..."
-python manage.py loaddata db_backup.json
-echo "Data successfully imported! Your new Postgres database now has all the old SQLite data."
