cf7a27278f225e1965679d8babaef5f933fcb413
sinan-prvt <mohamedsinan9400@gmail.com>
2026-09-20 20:07:11 +0530

    Update

--- frontend/src/components/themes/college-fest/layouts/TechFestModernLayout.tsx
+++ frontend/src/components/themes/college-fest/layouts/TechFestModernLayout.tsx
@@ -269,7 +269,12 @@ export default function TechFestModernLayout({ content, website, updateContent, 
                           <h3 className="text-2xl font-bold mb-4">{title}</h3>
                           <p className="text-gray-400 leading-relaxed">
                             {description}
-                          </p>
+                          </psrstrstststrsy
+                          ;kkl;;k;
+                          k
+                          ;key;key;k;
+                          keyk
+                          >
                           <button className={`mt-6 text-sm font-bold flex items-center gap-2 ${c.accentText} group-hover:gap-4 transition-all`}>
                             View Details <ExternalLink size={16} />
                           </button>
